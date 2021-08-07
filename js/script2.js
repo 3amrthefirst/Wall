7878c4430b0a30a8f2fed02b4057f90d814d4d96
3amrthefirst <amrrrnasser405@gmail.com>
2021-08-07 18:43:38 +0200

    amr
    final amr

---
+++ js/script2.js
@@ -0,0 +1,16 @@
+$('.slider').owlCarousel({
+    loop:true,
+    margin:10,
+    items:1,
+    dots:false,
+    URLhashListener:true
+  })
+  $('.slider2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:4,
+    dots:false,
+    center: true,
+    URLhashListener:true
+  })
